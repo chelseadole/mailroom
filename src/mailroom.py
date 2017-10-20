4e8e433f92836f90854173ae439b49f8b2c56ed5
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-20 13:57:11 -0700

    writing send_thankyou_email function, a function that then sends the user to ask_donation_amount.

--- src/mailroom.py
+++ src/mailroom.py
@@ -18,19 +18,66 @@ DONOR_DICT = {
 
 def initial_prompt():
     print("""
-    1.Send Thank You Email
-    2.View Donor Report
-    3.Exit/Quit
+
+    Ramson-Dole Foundation Donor Database. 
+    Follow the prompt, or type 'Quit' to exit
+    the program at any point.
+
+    \n 1.Send Thank You Email
+    \n 2.View Donor History Report
+    \n 3.Exit/Quit
     """)
 
-    initial_response = raw_input("What would you like to do? ") 
+    initial_prompt_response = raw_input('Enter 1, 2, or 3')
 
-    if initial_response == 1: 
-      send_thankyou_email()
-    elif initial_response == 2:
-      create_donor_report()
-    elif initial_response == 3:
-      # EXIT PROGRAM
+    if initial_prompt_response == 1:
+        send_thankyou_email()
+    elif initial_prompt_response == 2:
+        create_donor_report()
+    elif initial_prompt_response.lower() == 'quit':
+        SystemExit
     else:
-      print('\n Invalid choice. Please type 1, 2, or 3.') 
-      initial_prompt()
+        print('\n Invalid choice. Please type 1, 2, or 3.')
+        initial_prompt()
+
+
+def send_thankyou_email():
+    print("""
+
+        Enter the full name of the donor to email, or type 'list' to \
+        show a list of donor names.
+
+    """)
+
+    thankyou_response = raw_input('Donor Name:')
+
+    if thankyou_response == 'list':
+        print(DONOR_DICT.keys())
+        send_thankyou_email()
+    elif thankyou_response in DONOR_DICT:
+        ask_donation_amount(thankyou_response)
+    elif thankyou_response not in DONOR_DICT:
+        DONOR_DICT[thankyou_response] = []
+        ask_donation_amount(thankyou_response)
+    elif thankyou_response.lower() == 'quit':
+        SystemExit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
