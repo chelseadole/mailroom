88c500c6261ab2390dc3e8fc821444ac48208c74
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-23 16:13:38 -0700

    adding test_mailroom function to test donation amount

--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -1,8 +1,26 @@
 """Tests for mailroom, program that generates thank you letters and reports."""
 
+import pytest
+
+from mailroom import DONOR_DICT
+
+DONOR_NAMES = [
+    ('list', '1'),
+    ('Flerg Blergington', 'donate'),
+    ('Chelsea Dole', 'donate')
+]
+
+DONOR_AMOUNT = [
+    ('Anne Hathaway', '-50', 'donate'),
+    ('Remy Mei', '0', 'donate'),
+    ('Barack Obama', '100', 'initial'),
+    ('Count Basie', '1000', 'initial'),
+    ('Duke Ellington', '79223', 'initial')
+]
+
 
 def test_main():
-    """Test that trigrammer outputs actual trigram text."""
+    """Test that main outputs actual trigram text."""
     from mailroom import main
     from mailroom import ask_donor_name
     from mailroom import ask_donation_amount
@@ -16,7 +34,32 @@ def test_dictionary():
     assert type(DONOR_DICT) == dict
 
 
-def test_ask_donor_name():
+@pytest.mark.parametrize('name, result', DONOR_NAMES)
+def test_ask_donor_name(name, result):
     """Testing that donor_name is correctly routed."""
     from mailroom import ask_donor_name
-    ask_donor_name()
+    if name == 'list':
+        assert ask_donor_name(name) == '1'
+    else:
+        assert ask_donor_name(name) == 'donate'
+        assert type(DONOR_DICT[name]) == list
+
+
+@pytest.mark.parametrize('donor_name, donor_amount, result', DONOR_AMOUNT)
+def test_ask_donation_amount(donor_name, donor_amount, result):
+    """Testing that donor_amount is calculating/redirecting."""
+    from mailroom import ask_donor_name
+    from mailroom import ask_donation_amount
+    if donor_name not in DONOR_DICT:
+        assert ask_donor_name(donor_name) == 'donate'
+    assert ask_donation_amount(donor_name, donor_amount) == result
+
+@pytest.mark.parametrize('donor_name, donor_amount, result', DONOR_AMOUNT)
+def test_ask_donation_amount(donor_name, donor_amount, result):
+    """Testing that donor_amount is calculating/redirecting."""
+    from mailroom import ask_donor_name
+    from mailroom import ask_donation_amount
+    if donor_name not in DONOR_DICT:
+        assert ask_donor_name(donor_name) == 'donate'
+    assert ask_donation_amount(donor_name, donor_amount) == result
+
